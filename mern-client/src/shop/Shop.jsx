da0a87e5e1292d53f9c4950535f492d3401e6b38
Soumyajit Das <soumyajitdhoni@gmail.com>
2024-06-11 14:05:17 +0530

    shop backend server

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -3,7 +3,7 @@ import { Card } from "flowbite-react";
 const Shop = () => {
     const [books,setBooks] = useState([]);
     useEffect( () => {
-        fetch("http://localhost:5000/all-books").then(res => res.json()).then( data => setBooks(data));
+        fetch("https://book-api-zeta.vercel.app/all-books").then(res => res.json()).then( data => setBooks(data));
     })
   return (
     <div className='mt-28 px-4 lg:px24'>
